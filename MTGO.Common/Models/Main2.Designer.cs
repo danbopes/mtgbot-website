144f860657c66f53b9fd559231f28536ecd928d2
Styles <counterstrike.styles@gmail.com>
2013-08-17 02:08:35 -0700

    Cleaned up useless stuff

--- MTGO.Common/Models/Main2.Designer.cs
+++ MTGO.Common/Models/Main2.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\Daniel\Documents\Visual Studio 2012\Projects\MTGO\MTGO.Common\Models\Main.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\admin\Source\Repos\MTGO\mtgbot\MTGO.Common\Models\Main.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
